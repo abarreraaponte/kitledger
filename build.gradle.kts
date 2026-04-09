aec200920722c8d98677ce133c819a4600c6c5a2
Alejandro Barrera Aponte <alebarrera76@gmail.com>
2026-04-08 20:02:35 -0400

    Streamline package versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,7 @@
-val argon2JvmVersion: String by project
-val auth0JwtVersion: String by project
-val exposedVersion: String by project
-val flywayVersion: String by project
-val hikaricpVersion: String by project
-val hopliteVersion: String by project
-val jdbcPostgresDriverVersion: String by project
-val konformVersion: String by project
-val kotlinVersion: String by project
-val logbackVersion: String by project
-val postgresDriverVersion: String by project
-val uuidGeneratorVersion: String by project
-
 plugins {
-    kotlin("jvm") version "2.2.20"
-    kotlin("plugin.serialization") version "2.2.20"
-    id("io.ktor.plugin") version "3.3.0"
+    kotlin("jvm") version "2.3.20"
+    kotlin("plugin.serialization") version "2.3.20"
+    id("io.ktor.plugin") version "3.4.0"
 }
 
 group = "com.kitledger"
@@ -25,30 +12,39 @@ application {
 }
 
 dependencies {
+    // Ktor Server
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty")
     implementation("io.ktor:ktor-server-status-pages")
     implementation("io.ktor:ktor-server-auth")
-    implementation("io.ktor:ktor-server-core")
-    implementation("io.ktor:ktor-server-config-yaml")
+    implementation("io.ktor:ktor-server-cors")
     implementation("io.ktor:ktor-server-content-negotiation")
     implementation("io.ktor:ktor-serialization-kotlinx-json")
-    implementation("com.auth0:java-jwt:$auth0JwtVersion")
-    implementation("ch.qos.logback:logback-classic:$logbackVersion")
-    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-jdbc:${exposedVersion}")
-    implementation("org.jetbrains.exposed:exposed-json:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposedVersion")
-    implementation("org.flywaydb:flyway-core:$flywayVersion")
-    implementation("org.flywaydb:flyway-database-postgresql:$flywayVersion")
-    implementation("com.zaxxer:HikariCP:${hikaricpVersion}")
-    implementation("org.postgresql:postgresql:${postgresDriverVersion}")
-    implementation("org.postgresql:postgresql:${jdbcPostgresDriverVersion}")
-    implementation("de.mkammerer:argon2-jvm:$argon2JvmVersion")
-    implementation("com.fasterxml.uuid:java-uuid-generator:$uuidGeneratorVersion")
-    implementation("io.konform:konform-jvm:$konformVersion")
-    implementation("com.sksamuel.hoplite:hoplite-core:$hopliteVersion")
-    implementation("com.sksamuel.hoplite:hoplite-hocon:$hopliteVersion")
+
+    // Auth & Logging
+    implementation("com.auth0:java-jwt:4.5.0")
+    implementation("ch.qos.logback:logback-classic:1.5.13")
+
+    // Database (Exposed + Flyway + Hikari)
+    implementation("org.jetbrains.exposed:exposed-core:1.0.0-rc-4")
+    implementation("org.jetbrains.exposed:exposed-jdbc:1.0.0-rc-4")
+    implementation("org.jetbrains.exposed:exposed-json:1.0.0-rc-4")
+    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:1.0.0-rc-4")
+    implementation("org.flywaydb:flyway-core:11.13.0")
+    implementation("org.flywaydb:flyway-database-postgresql:11.13.0")
+    implementation("com.zaxxer:HikariCP:7.0.2")
+    implementation("org.postgresql:postgresql:42.7.7")
+
+    // Utilities
+    implementation("de.mkammerer:argon2-jvm:2.12")
+    implementation("com.fasterxml.uuid:java-uuid-generator:5.1.0")
+    implementation("io.konform:konform-jvm:0.11.1")
+
+    // Config (Hoplite)
+    implementation("com.sksamuel.hoplite:hoplite-core:3.0.0.RC3")
+    implementation("com.sksamuel.hoplite:hoplite-hocon:3.0.0.RC3")
+
+    // Testing
     testImplementation("io.ktor:ktor-server-test-host")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:2.3.20")
 }
